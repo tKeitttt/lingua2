eee5a29a364f58e9f04b1934263c4140b6c59b82
tKeitttt <oakeisuke0308@gmail.com>
2021-05-23 01:51:34 +0900

    サインイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configre_permitted_parameters, if: :devise_controller?
+
+  def configre_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:username, :nationality, :birthday, :profile])
+  end
 end

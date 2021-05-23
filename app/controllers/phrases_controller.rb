99b4955225e908662870a1d75b992e9461825e6c
tKeitttt <oakeisuke0308@gmail.com>
2021-05-23 11:01:34 +0900

    phrasesコントローラー準備

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,5 +1,19 @@
 class PhrasesController < ApplicationController
+  
   def progress
     @progress="Hello"
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def search
+  end
+
+  def exrecise
+  end
+
 end

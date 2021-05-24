925046a234a75d564ea52428a479dd54e4edf949
tKeitttt <oakeisuke0308@gmail.com>
2021-05-24 11:36:55 +0900

    ユーザー詳細、一覧表示機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_23_032533) do
+ActiveRecord::Schema.define(version: 2021_05_24_023509) do
 
   create_table "phrase_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "phrase_id"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2021_05_23_032533) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_phrases_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "roomname", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

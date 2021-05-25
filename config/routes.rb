d0bac86e2a8f8e5996b4c24ee7f9c1b290986d62
tKeitttt <oakeisuke0308@gmail.com>
2021-05-25 17:18:20 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"phrases#progress"
 
-  resources :users, only: [:edit,:update,:index,:show]
+  resources :users, only: [:index,:show,:edit,:update]
   
   resources :phrases, only: [:new, :create,:edit, :update] do
     collection do
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :rooms, only: [:index,:new,:create] do
+    resources :messages, only: [:index, :create]
+  end
+
   resources :articles
-  resources :rooms, only: [:new,:create]
+  
 end
